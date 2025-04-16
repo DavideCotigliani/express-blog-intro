17c07a415dbc7549f877a016efceb9bfdabb9c60
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-16 14:47:26 +0200

    [UPD] res.send ('server del mio blog') and test Postman completed

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+//creo il server Express
+// importo express
+const express = require('express')
+// inizializzo express e lo salvo in una variabile
+const app = express()
+// definisco il numero di porta
+const port= 4000
+
+//definisco la prima rotta
+app.get('/',(req,res)=>{
+res.send('Server del mio blog')
+})
+
+//metto in ascolto il server alla porta indicata
+app.listen(port,()=>{
+    console.log(`il server è in ascolto presso la porta ${port}`); 
+})
