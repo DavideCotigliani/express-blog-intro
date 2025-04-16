4b50bf031cfde3a65b67751423303d4bd4c90b46
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-16 15:27:35 +0200

    [UPD] Created the other adress "bacheca"

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//creo il server Express
 // importo express
 const express = require('express')
 // inizializzo express e lo salvo in una variabile
@@ -10,39 +9,44 @@ const port= 4000
 app.get('/',(req,res)=>{
 console.log('Server del mio blog');
 })
-//creo il mio array
-const myArray = [
-    {
-        titolo: 'ciambellone',
-        contenuto: 'lorem ipsum',
-        immagine: 'immagini/ciambellone.jpeg',
-        tags: ['buono', 'gustoso','delizioso']
-    },
-    {
-        titolo: 'cracker barbabietola',
-        contenuto: 'lorem ipsum',
-        immagine: 'immagini/cracker_barbabietola.jpeg',
-        tags: ['friabile', 'super','delizioso']
-    },
-    {
-        titolo: 'pane fritto',
-        contenuto: 'lorem ipsum',
-        immagine: 'immagini/pane_fritto_dolce.jpeg',
-        tags: ['dorato', 'ottimo','delizioso']
-    },
-    {
-        titolo: 'pasta barbabietola',
-        contenuto: 'lorem ipsum',
-        immagine: 'immagini/pasta_barbabietola.jpeg',
-        tags: ['saziante', 'fantastica','delizioso']
-    },
-    {
-        titolo: 'torta paesana',
-        contenuto: 'lorem ipsum',
-        immagine: 'immagini/torta_paesana.jpeg',
-        tags: ['tradizionale', 'indimenticabile','delizioso']
-    }
-]
+
+//creo la rotta bacheca
+app.get('/bacheca', (req,res)=>{
+    const myArray = [
+        {
+            titolo: 'ciambellone',
+            contenuto: 'lorem ipsum',
+            immagine: 'immagini/ciambellone.jpeg',
+            tags: ['buono', 'gustoso','delizioso']
+        },
+        {
+            titolo: 'cracker barbabietola',
+            contenuto: 'lorem ipsum',
+            immagine: 'immagini/cracker_barbabietola.jpeg',
+            tags: ['friabile', 'super','aperitivo']
+        },
+        {
+            titolo: 'pane fritto',
+            contenuto: 'lorem ipsum',
+            immagine: 'immagini/pane_fritto_dolce.jpeg',
+            tags: ['dorato', 'ottimo','classico']
+        },
+        {
+            titolo: 'pasta barbabietola',
+            contenuto: 'lorem ipsum',
+            immagine: 'immagini/pasta_barbabietola.jpeg',
+            tags: ['saziante', 'fantastica','sfiziosa']
+        },
+        {
+            titolo: 'torta paesana',
+            contenuto: 'lorem ipsum',
+            immagine: 'immagini/torta_paesana.jpeg',
+            tags: ['tradizionale', 'indimenticabile','travolgente']
+        }
+    ];
+    res.json(myArray);
+})
+
 //metto in ascolto il server alla porta indicata
 app.listen(port,()=>{
     console.log(`il server è in ascolto presso la porta ${port}`); 
